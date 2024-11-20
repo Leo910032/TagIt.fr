42a5b744e7db122dadf2b52020da03c563fde8f9
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2024-11-20 08:09:19 +0000

    Create sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://mylinks.fabiconcept.online',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://mylinks.fabiconcept.online/signup',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://mylinks.fabiconcept.online/login',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://mylinks.fabiconcept.online/freepalestine',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://mylinks.fabiconcept.online/fabiconcept',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+    ]
+}
