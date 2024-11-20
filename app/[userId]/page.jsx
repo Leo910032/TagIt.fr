7c53e00490143f7d620a55956f8a0cc46436e1be
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2024-11-20 08:03:32 +0000

    Redirect invalid userId to 404 page

--- app/[userId]/page.jsx
+++ app/[userId]/page.jsx
@@ -4,19 +4,27 @@ import { collection, doc, getDoc } from "firebase/firestore";
 import House from "./House";
 import Filter from "bad-words"
 import { Toaster } from "react-hot-toast";
-export async function generateMetadata ({ params: { userId } }) {
-    const filter = new Filter();
-    const currentUser = await fetchUserData(userId);;
-    const collectionRef = collection(fireApp, "AccountData");
-    const docSnap = await getDoc(doc(collectionRef, `${currentUser}`));
+import { notFound } from 'next/navigation';
 
-    if (docSnap.exists()) {
-        const { metaData } = docSnap.data();
-        
-        return ({
-            title: metaData && metaData.title ? filter.clean(metaData.title) :`@${userId} Landing Page`,
-            description: metaData && metaData.description ? filter.clean(metaData.description) :``,
-        });
+export async function generateMetadata ({ params: { userId } }) {
+    try {
+        const filter = new Filter();
+        const currentUser = await fetchUserData(userId);;
+        const collectionRef = collection(fireApp, "AccountData");
+        const docSnap = await getDoc(doc(collectionRef, `${currentUser}`));
+    
+        if (docSnap.exists()) {
+            const { metaData } = docSnap.data();
+            
+            return ({
+                title: metaData && metaData.title ? filter.clean(metaData.title) :`@${userId} Landing Page`,
+                description: metaData && metaData.description ? filter.clean(metaData.description) :``,
+            });
+        }else {
+            notFound();
+        }
+    } catch (error) {
+        notFound();
     }
 };
 
