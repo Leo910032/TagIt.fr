c2063f0804b19a84bada6ab824ccb9e59e0da2c7
fabiconcept <favourajokubi@gmail.com>
2024-11-28 11:58:26 +0100

    Add bgType and themeTextColour states to SupportBanner component, update useEffect to fetch additional data from docSnap, and apply themeTextColour to banner text color

--- app/[userId]/components/SupportBanner.jsx
+++ app/[userId]/components/SupportBanner.jsx
@@ -12,6 +12,8 @@ export default function SupportBanner({ userId }) {
     const [supportGroup, setSupportGroup] = useState(0);
     const [supportGroupStatus, setSupportGroupStatus] = useState(false);
     const [expanded, setExpanded] = useState(false);
+    const [bgType, setBgType] = useState("");
+    const [themeTextColour, setThemeTextColour] = useState("");
 
     useEffect(() => {
         async function fetchProfilePicture() {
@@ -21,9 +23,11 @@ export default function SupportBanner({ userId }) {
 
             onSnapshot(docRef, (docSnap) => {
                 if (docSnap.exists()) {
-                    const { supportBanner, supportBannerStatus } = docSnap.data();
+                    const { themeFontColor, selectedTheme, supportBanner, supportBannerStatus } = docSnap.data();
                     setSupportGroup(supportBanner ? supportBanner : 0);
                     setSupportGroupStatus(supportBannerStatus ? supportBannerStatus : false);
+                    setBgType(selectedTheme);
+                    setThemeTextColour(themeFontColor ? themeFontColor : "");
 
                     setTimeout(() => {
                         setExpanded(true);
@@ -37,7 +41,9 @@ export default function SupportBanner({ userId }) {
     return (
         <>
             {supportGroupStatus && <div className="fixed bottom-0 w-screen left-0 z-[100]">
-                <div className="py-4 px-6 bg-black absolute left-0 w-full bottom-0 text-white banner flex flex-col items-center">
+                <div className="py-4 px-6 bg-black absolute left-0 w-full bottom-0 text-white banner flex flex-col items-center border-t border-t-green-400/50 shadow-xl" style={{
+                    color: themeTextColour
+                }}>
                     <div className={`filter invert ${expanded ? "" : "rotate-180"} top-6 absolute right-6 cursor-pointer`}>
                         <Image
                             src={"https://linktree.sirv.com/Images/icons/arr.svg"}
